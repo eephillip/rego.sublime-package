953358a08f56cbb21397dd6e60e40e27fe4b8ea2
eephillip <eephillip@eephillip.com>
2020-04-22 13:59:00 -0500

    Using sublime-syntax from OPA Submodule

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ DIST="${SCRIPT_PATH}/dist/Rego.sublime-package"
 rm -rf "${DIST}"
 
 pushd ./package
+
+# Copy the  newly available sublime-syntax from the OPA submodule
+cp -f ./opa/misc/syntax/sublime/rego.sublime-syntax Rego.sublime-syntax
+
 zip -r "${DIST}" .
 popd
 unzip -l "${DIST}"
